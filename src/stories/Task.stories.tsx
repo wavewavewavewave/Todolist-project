82dd9b2f4b1e1dcddd1a85ad2a08d7314d6506e1
wavewavewavewave <nikitashkrabov@gmail.com>
2021-12-05 14:52:53 +0300

    work with sb

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+import {AddItemForm} from "../AddItemForm";
+import {action} from "@storybook/addon-actions";
+import {Task} from "../Task";
+import {TaskType} from "../Todolist";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TODOLIST/Task',
+  component: Task,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  args: {
+    removeTask: action('removeTask'),
+    changeTaskStatus: action('changeTaskStatus'),
+    changeTaskTitle: action('changeTaskTitle')
+  }
+} as ComponentMeta<typeof Task>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+
+export const TaskIsDoneStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskIsDoneStory.args = {
+  task: {id: '122', isDone: true, title: 'JS' },
+};
+
+export const TaskIsNotDoneStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskIsNotDoneStory.args = {
+  task: {id: '1222', isDone: false, title: 'React' },
+};
