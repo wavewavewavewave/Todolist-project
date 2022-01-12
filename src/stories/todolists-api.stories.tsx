a4b1237f1c31b1ba3cdb6772abf685bacde1c697
wavewavewavewave <nikitashkrabov@gmail.com>
2022-01-12 21:38:36 +0300

    .get, .post

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,12 +1,22 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
+}
+const settings = {
+    withCredentials: true,
+    headers: {
+        'API-KEY': '911248f0-1866-498d-84bc-ce9553ace487'
+    }
 }
 
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings).then((res) => {
+            setState(res.data)
+        })
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
 
@@ -17,6 +27,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: "newTodolist"}, settings).then((res) => {
+            setState(res.data)
+
+        })
+
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
