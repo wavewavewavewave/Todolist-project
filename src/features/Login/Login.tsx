16dc1e7f6882a6e9a5dc49077ad0f363f4cd654f
wavewavewavewave <nikitashkrabov@gmail.com>
2022-04-02 21:06:26 +0300

    refactor getFieldProps

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -60,21 +60,15 @@ export const Login = () => {
                 <FormGroup>
                     <TextField label="Email"
                                margin="normal"
-                               name="email"
-                               onChange={formik.handleChange}
-                               onBlur={formik.handleBlur}
-                               value={formik.values.email}/>
+                               {...formik.getFieldProps("email")}/>
                     {formik.touched.email && formik.errors.email
                     && <div style={{color: 'red'}}>{formik.errors.email}</div>}
 
                     <TextField type="password"
                                label="Password"
                                margin="normal"
-                               name="password"
-                               onChange={formik.handleChange}
-                               onBlur={formik.handleBlur}
-                               value={formik.values.password}
-                    />
+                               {...formik.getFieldProps("password")}/>
+
                     { formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
                     <FormControlLabel label={'Remember me'} control={
                         <Checkbox name="rememberMe"
