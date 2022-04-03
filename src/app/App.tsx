3c4de118a05f80962c621f453a3a216195cd8ef0
wavewavewavewave <nikitashkrabov@gmail.com>
2022-04-03 20:26:42 +0300

    routes and Login component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,8 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import { Menu } from '@mui/icons-material';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
 
 type PropsType = {
     demo?: boolean
@@ -36,7 +38,12 @@ function App({demo = false}: PropsType) {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList demo={demo}/>
+                <Routes>
+                    <Route path="/" element={<TodolistsList demo={demo}/>}/>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="/404" element={<h1 style={{textAlign: "center"}}>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to={"/404"}/>}/>
+                </Routes>
             </Container>
         </div>
     )
